ad009fbf871b22b487b806f1d2722587b7d6975e
Hridaya <hridayahoney@gmail.com>
2021-02-06 15:31:20 +0530

    Create CoinType.java

---
+++ Java Solutions/Coin Counter/CoinType.java
@@ -0,0 +1,16 @@
+package CoinCounter.src;
+
+public enum CoinType {
+    ONE,
+    TWO,
+    FIVE,
+    TEN,
+    TWENTY,
+    FIFTY,
+    ONE_LARGE,
+    TWO_LARGE,
+    FIVE_NOTE,
+    TEN_NOTE,
+    TWENTY_NOTE,
+    FIFTY_NOTE
+}
